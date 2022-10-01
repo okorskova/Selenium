b6b9e58d706e83e22c68569c51ff7c72c639f2bb
Olga Korskova <o-korskova@yandex.ru>
2022-10-01 21:59:28 +0300

    Modified ApplicationTest

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,7 +20,7 @@ class ApplicationTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
